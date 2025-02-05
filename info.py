d3115289a00fd5b25b6a0643548699efa665c2ec
shivamkr-98 <shivamjnv2305@gmail.com>
2025-02-05 16:55:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ from Script import script
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJ')
+SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '27732828'))
 API_HASH = environ.get('API_HASH', '1586630ea98d2393f7feb51cb84c3121')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
